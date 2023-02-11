641881561b61d72af5d93ac79cb1d3cca962a4e2
khalil <khalillakhdharatc@gmail.com>
2023-02-11 14:11:23 +0100

    Initial commit

---
+++ if.py
@@ -0,0 +1,11 @@
+age=int(input("Entrez votre age:"))
+if age<18:
+    print("vous êtes mineur")
+    print("vous n'êtes pas autorisée")
+elif age<=65:
+    print("vous êtes majeur")
+else:
+    print("vous êtes sénior")
+
+    
+    
